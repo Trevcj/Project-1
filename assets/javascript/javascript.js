ef4b97eda9b0a1b0b6749efde29b262daaaddf22
rubib <rubib@email.arizona.edu>
2017-12-09 13:32:39 -0700

    javascript files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,44 @@
-//----------------GOOGLE DIRECTIONS---------------------
-var directionsAPI
+
+//--------------GOOGLE MAPS ---------------------
+   var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
 
+//----------------DIRECTIONS API---------------------
+var startingLocation="Yuma, Arizona";
+var endingLocation="Tucson, Arizona";
+
+function directionsURL(startingLocation, endingLocation){
+	var directionsKEY="AIzaSyAfNedlP-Xv-cl6ni8nbDMZD_red3X08WI";
+
+
+	var directionsURL="https://maps.googleapis.com/maps/api/directions/json?origin="+startingLocation+"&destination="+endingLocation+"&key="+directionsKEY;
+	return directionsURL;
+
+}
+
+function getDirectionsAPI(){
+
+	$.ajax({
+		url: directionsURL(startingLocation,endingLocation),
+		method:"GET"
+	})
+	.done(function(response){
+		
+		console.log(response.routes[0].legs.distance.text);
+	
+	})
+}
+
+getDirectionsAPI();
 
 //------------------WEATHER API-----------------------------------
 
-var geoCodeAPI;
 function undergroundWeatherURL(longitude,latitude){
 
 	var undergroundWeatherapiKey="b26eea70cef99b97";
@@ -15,23 +48,23 @@ function undergroundWeatherURL(longitude,latitude){
 	return undergroundWeatherURL;
 }
 
-function getundergroundWeatherAPI(){
+function getUndergroundweatherAPI(){
 
 	$.ajax({
+		//makesure you change this when user inputs
 		url: undergroundWeatherURL(32.2217,-110.9265),
 		method:"GET"
 	})
 	.done(function(response){
-		
-		return response;
+
+		//checking to see if it returns values
+		//console.log(response.hourly_forecast[0].FCTTIME.hour);
 	
 	})
 }
-var undergroundWeatherAPI=getundergroundWeatherAPI();
 
-undergroundWeatherAPI.
-
-console.log( undergroundWeatherURL(32.2217,-110.9265));
+//makesure you call the funtion to display 
+getUndergroundweatherAPI();
 
 
 
