e26ea7fa4d6af8fb1070d75875ccbc5d9db00188
rubib <rubib@email.arizona.edu>
2017-12-09 11:30:41 -0700

    updating my copy from master

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,26 +1,38 @@
-var undergroundWeatherAPI;
+//----------------GOOGLE DIRECTIONS---------------------
+var directionsAPI
+
+
+
+//------------------WEATHER API-----------------------------------
+
 var geoCodeAPI;
-
-
 function undergroundWeatherURL(longitude,latitude){
 
-	var undergroundWeatherapiKey=b26eea70cef99b97;
+	var undergroundWeatherapiKey="b26eea70cef99b97";
 
 	var undergroundWeatherURL="http://api.wunderground.com/api/"+undergroundWeatherapiKey+"/hourly/q/"+longitude+","+latitude+".json";
 
 	return undergroundWeatherURL;
 }
 
-function undergroundWeatherAPI(){
+function getundergroundWeatherAPI(){
 
 	$.ajax({
 		url: undergroundWeatherURL(32.2217,-110.9265),
 		method:"GET"
 	})
 	.done(function(response){
+		
 		return response;
+	
 	})
 }
+var undergroundWeatherAPI=getundergroundWeatherAPI();
+
+undergroundWeatherAPI.
+
+console.log( undergroundWeatherURL(32.2217,-110.9265));
+
 
 
 
