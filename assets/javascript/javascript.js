e394cf78024fc55e7e84d8f75e98d8b8a53d4e33
rubib <rubib@email.arizona.edu>
2017-12-20 20:52:48 -0700

    added cors and s for http icon image

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -109,7 +109,7 @@ function calcWaypointTime(toWaypointdurationMin,toWaypointdurationHr,currentTime
 function directionsAPI(originLat,originLng,markerPositionLat,markerPositionLng){
   var directionsKEY="AIzaSyAfNedlP-Xv-cl6ni8nbDMZD_red3X08WI";
   //trevor's backup AIzaSyAIq7MXbfsfyh18by7GqjrtP7xKeFmR-e8
-  var directionsURL="https://cors-anywhere.herokuapp.com/" +"https://maps.googleapis.com/maps/api/directions/json?origin="+originLat+","+originLng+"&destination="+markerPositionLat+","+markerPositionLng+"&key="+directionsKEY;
+  var directionsURL="https://cors-anywhere.herokuapp.com/"+"https://maps.googleapis.com/maps/api/directions/json?origin="+originLat+","+originLng+"&destination="+markerPositionLat+","+markerPositionLng+"&key="+directionsKEY;
 //  console.log(directionsURL);
   return $.ajax({
     url: directionsURL,
